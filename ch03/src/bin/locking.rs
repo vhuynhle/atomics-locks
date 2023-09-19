4a26000401f62671fdf0e23821ad0fc8a288f2cc
Van Huynh Le <v.h.le@protonmail.com>
2023-09-19 22:00:07 +0200

    Use AtomicBool with Acquire-Release order as a lock

---
+++ ch03/src/bin/locking.rs
@@ -0,0 +1,32 @@
+use std::{
+    sync::atomic::{AtomicBool, Ordering},
+    thread,
+};
+
+static mut DATA: String = String::new();
+static LOCKED: AtomicBool = AtomicBool::new(false);
+
+fn f() {
+    if LOCKED
+        .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+        .is_ok()
+    {
+        // Safety: We're holding the exclusive lock, so nothing else is accessing DATA.
+        unsafe {
+            DATA.push('!');
+        }
+        LOCKED.store(false, Ordering::Release);
+    }
+}
+
+fn main() {
+    thread::scope(|s| {
+        for _ in 0..100 {
+            s.spawn(f);
+        }
+    });
+
+    unsafe {
+        println!("{}", DATA);
+    }
+}
