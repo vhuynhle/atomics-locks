21acbbffb05c40fb02696f8fb060997fc1182d18
Van Huynh Le <v.h.le@protonmail.com>
2023-09-30 13:31:40 +0200

    A spin lock with unsafe interface

---
+++ ch04/src/bin/spinlock_2.rs
@@ -0,0 +1,37 @@
+use std::{cell::UnsafeCell, sync::atomic::AtomicBool};
+use std::sync::atomic::Ordering;
+
+pub struct SpinLock<T> {
+    locked: AtomicBool,
+    value: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for SpinLock<T>
+    where T: Send
+{}
+
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn lock(&self) -> &mut T {
+        while self.locked.swap(true, Ordering::Acquire) {
+            std::hint::spin_loop();
+        }
+        unsafe {
+            &mut *self.value.get()
+        }
+    }
+
+    /// Safety: The &mut T from lock() must be gone before this function
+    /// can be called.
+    pub unsafe fn unlock(&self) {
+        self.locked.store(false, Ordering::Release);
+    }
+}
+
+fn main() {}
